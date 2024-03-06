88e296cd48faca2a2203c68f55f2310761119d6b
Wendel Santos <contact.wendel@gmail.com>
2024-03-06 13:22:43 -0300

    :tada: Initial Commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+            '@mocks': './src/mocks',
+            '@services': './src/services',
+          },
+        },
+      ],
+    ],
+  };
 };
