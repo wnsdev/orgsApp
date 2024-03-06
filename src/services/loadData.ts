3947847d82ab2cf78a6f0fcf70c28579f36e934f
Wendel Santos <contact.wendel@gmail.com>
2024-03-06 13:30:49 -0300

    feat: Adding Services Folder

---
+++ src/services/loadData.ts
@@ -0,0 +1,10 @@
+import {producers} from '@mocks/producers';
+import {top} from '@mocks/top';
+
+export function loadProducers() {
+  return producers;
+}
+
+export function loadTop() {
+  return top;
+}
